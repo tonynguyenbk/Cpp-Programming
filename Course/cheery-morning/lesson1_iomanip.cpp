df01be5c101dee919667abaaddf143909c459777
tonynguyenbk <truong.nd172877@sis.hust.edu.vn>
2023-07-13 23:20:31 +0700

    add new courses

---
+++ Course/cheery-morning/lesson1_iomanip.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+    float a = 3.9298;
+    cout << fixed << setprecision(2) << a << endl;
+    return 0;
+}
--- helloworld.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main() {
-    printf("Hello world from my Thinkpad T430 ;)");
-    return 0;
-}
